70c82c1c1533c954b14f3abecec4882b615bbc1e
Tushar Sharma <tusharmahalya@gmail.com>
2023-03-01 03:52:00 +0530

    code optimization

--- models.py
+++ models.py
@@ -1,35 +1,57 @@
-model_1 = models.Sequential(name = 'Over_Fitted_Model')
-model_1.add(layers.Dense(100, input_shape = (784,),
-                        activation = 'relu',
-                        kernel_initializer = RandomNormal(mean=0.0, stddev=1, seed=0),
-                        name = 'hidden_1'))
-model_1.add(layers.Dense(100, activation = 'relu',
-                        kernel_initializer = RandomNormal(mean=0.0, stddev=1, seed=0),
-                        name = 'hidden_2'))
-model_1.add(layers.Dense(100, activation = 'relu',
-                        kernel_initializer = RandomNormal(mean=0.0, stddev=1, seed=0),
-                        name='hidden_3'))
-model_1.add(layers.Dense(1, activation='sigmoid',
-                        kernel_initializer = RandomNormal(mean=0.0, stddev=1, seed=0),
-                        name='output'))
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.regularizers import L2
+from keras.initializers import RandomNormal
+from keras.layers import Dense, Dropout, BatchNormalization
 
-model_1.summary()
 
-model_2 = models.Sequential(name = 'Regularized_Model')
-L2_reg = regularizers.L2(0.00006)
-model_2.add(Dropout(0.2, input_shape=(784,)))
-model_2.add(layers.Dense(100, input_shape = (784,),
-                         activation = 'relu',
-                         kernel_regularizer = L2_reg,
-                         name = 'hidden_1'))
-model_2.add(layers.BatchNormalization())
-model_2.add(layers.Dense(100, activation = 'relu',
-                         kernel_regularizer = L2_reg,
-                         name='hidden_2'))
-model_2.add(Dropout(0.10))
-model_2.add(layers.Dense(100, activation='relu',
-                         kernel_regularizer= L2_reg,
-                         name='hidden_3'))
-model_2.add(layers.Dense(1, activation='sigmoid', name='output'))
+def Over_Fitted_Model():
+    model = Sequential(
+        [
 
-model_2.summary()
+            Dense(100, input_shape=(784,), activation='relu',
+                  kernel_initializer=RandomNormal(mean=0.0, stddev=1, seed=0),
+                  name='hidden_1'),
+            Dense(100, activation='relu', kernel_initializer=RandomNormal(mean=0.0, stddev=1, seed=0),
+                  name='hidden_2'),
+            Dense(100, activation='relu', kernel_initializer=RandomNormal(mean=0.0, stddev=1, seed=0),
+                  name='hidden_3'),
+            Dense(1, activation='sigmoid', kernel_initializer=RandomNormal(mean=0.0, stddev=1, seed=0),
+                  name='output'),
+        ],
+        name='Over_Fitted_Model'
+    )
+    model.compile(optimizer=Adam(learning_rate=0.001),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+    return model
+
+
+def Regularized_model():
+    model = Sequential(
+        [
+            Dropout(0.2, input_shape=(784,)),
+            Dense(100, input_shape=(784,),
+                  activation='relu',
+                  kernel_regularizer=L2(0.00006),
+                  name='hidden_1'),
+            BatchNormalization(),
+            Dense(100, activation='relu',
+                  kernel_regularizer=L2(0.00006),
+                  name='hidden_2'),
+            Dropout(0.10),
+            Dense(100, activation='relu',
+                  kernel_regularizer=L2(0.00006),
+                  name='hidden_3'),
+            Dense(1, activation='sigmoid', name='output'),
+        ],
+        name='Regularized_Model'
+    )
+    model.compile(optimizer=Adam(learning_rate=0.001),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+    return model
